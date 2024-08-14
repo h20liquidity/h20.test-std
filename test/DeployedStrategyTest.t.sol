6205b644e48c279eeaa5b6ba1bd911c6621fb7f1
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-08-14 19:47:53 +0530

    fix

--- test/DeployedStrategyTest.t.sol
+++ test/DeployedStrategyTest.t.sol
@@ -127,7 +127,7 @@ contract DeployedStrategyTest is StrategyTests {
             vm.recordLogs();
 
             // `arb()` called
-            takeArbOrder(order, strategy.takerRoute, strategy.inputTokenIndex, strategy.outputTokenIndex);
+            takeArbOrder(order, strategy.takerRoute, strategy.inputTokenIndex, strategy.outputTokenIndex, new SignedContextV1[](0));
 
             Vm.Log[] memory entries = vm.getRecordedLogs();
             (uint256 strategyAmount, uint256 strategyRatio) = getCalculationContext(entries);
