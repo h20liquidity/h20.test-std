3a738d25732331c814afc7cb564e2a82fc7514c9
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-08-27 21:30:15 +0530

    bump flake

--- flake.nix
+++ flake.nix
@@ -11,8 +11,32 @@
       let
         pkgs = rainix.pkgs.${system};
       in rec {
-        packages = rainix.packages.${system};
-        devShells = rainix.devShells.${system};
+        packages = rec{
+
+          network-list = rainix.network-list.${system};
+
+          rainix-deployer-words = rainix.mkTask.${system} {
+            name = "rainix-deployer-words";
+            body = ''
+              for network in ${network-list}
+              do
+                echo "Checking deployer words for $network"
+                cargo run --manifest-path ''${MANIFEST_PATH} --package rain_orderbook_cli words -c ''${SETTINGS_PATH} -d "$network" --stdout
+              done
+            '';
+          };
+        } // rainix.packages.${system};
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            packages.rainix-deployer-words
+          ];
+
+          shellHook = rainix.devShells.${system}.default.shellHook;
+          buildInputs = rainix.devShells.${system}.default.buildInputs;
+          nativeBuildInputs = rainix.devShells.${system}.default.nativeBuildInputs;
+        };
+
       }
     );
 
